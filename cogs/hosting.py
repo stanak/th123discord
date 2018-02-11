9dd857e983f0770ba95f092076bdb01e95194909
starnak <stip2222@gmail.com>
2018-02-11 18:13:33 +0900

    ホスト自動判別の原型作成

--- cogs/hosting.py
+++ cogs/hosting.py
@@ -5,19 +5,64 @@ from discord.ext import commands
 import discord
 import unicodedata
 import asyncio
+import struct
+import socket
 
 import logging
 
 logger = logging.getLogger(__name__)
 
-_HOST_MESSAGE_SEC = 3600
-_DEFAULT_MESSAGE_SEC = 60
-_SHORT_MESSAGE_SEC = 10
+s = struct.Struct("!"+"B"*37)
+binary_data = [0x01, 0x02, 0x00, 0x2a, 0x30, 0x8c,
+        0x71, 0x3e, 0xeb, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x2a, 0x30, 0x8c,
+        0x71, 0x3e, 0xeb, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x77, 0xbc]
+PACKET_TO_HOST = s.pack(*binary_data)
+
+
+# cheetsheets https://www.pythonsheets.com/notes/python-asyncio.html
+def recvfrom(loop, sock, n_bytes, fut=None, registed=False):
+    fd = sock.fileno()
+    if fut is None:
+        fut = loop.create_future()
+    if registed:
+        loop.remove_reader(fd)
+
+    try:
+        data, addr = sock.recvfrom(n_bytes)
+    except (BlockingIOError, InterruptedError):
+        loop.add_reader(fd, recvfrom, loop, sock, n_bytes, fut, True)
+    else:
+        fut.set_result((data, addr))
+    return fut
+
+def sendto(loop, sock, data, addr, fut=None, registed=False):
+    fd = sock.fileno()
+    if fut is None:
+        fut = loop.create_future()
+    if registed:
+        loop.remove_writer(fd)
+    if not data:
+        return
+
+    try:
+        n = sock.sendto(data, addr)
+    except (BlockingIOError, InterruptedError):
+        loop.add_writer(fd, sendto, loop, sock, data, addr, fut, True)
+    else:
+        fut.set_result(n)
+    return fut
+
+def host_status(packet, is_sokuroll=False):
+    if packet == b'\x03':
+        return 1
+    else:
+        return 0
 
 
 class Hosting(CogMixin):
     def __init__(self, bot):
         self.bot = bot
+
 
     @commands.command(pass_context=True)
     async def host(self, ctx, ip_port: str, *comment):
@@ -28,18 +73,39 @@ class Hosting(CogMixin):
         募集例「!host 123.456.xxx.xxx:10800 霊夢　レート1500　どなたでもどうぞ！」
         """
         normalized_host = unicodedata.normalize('NFKC', ip_port)
+        ip, port = normalized_host.split(":")
         host_message = normalized_host + " | " + " ".join(comment)
         user = ctx.message.author
         hostlist_ch = discord.utils.get(self.bot.get_all_channels(),
                                            name="hostlist")
+
         not_private = not ctx.message.channel.is_private
         if not_private:
             await self.bot.delete_message(ctx.message)
             raise errors.OnlyPrivateMessage
 
         await self._delete_messages_from(hostlist_ch, user)
-        await self.bot.post(hostlist_ch, "{0.mention}, {1}".format(user, host_message), delete_after=_HOST_MESSAGE_SEC)
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.setblocking(False)
+        message = await self.bot.send_message(hostlist_ch, "{0.mention}, {1}".format(user, host_message))
         await self.bot.whisper("募集を開始しました。")
+        count = 0
+        while(count <= 10):
+            await sendto(self.bot.loop, sock, PACKET_TO_HOST, (ip, int(port)))
+            await asyncio.sleep(3)
+            data, _ = await recvfrom(self.bot.loop, sock, 1024)
+            status = host_status(data)
+            if status == 1:
+                count = 0
+                message = await self.bot.edit_message(message, "{0.mention}, :o: {1}".format(user, host_message))
+            elif status == 0:
+                count += 1
+                message = await self.bot.edit_message(message, "{0.mention}, :x: {1}".format(user, host_message))
+        await self._delete_messages_from(hostlist_ch, user)
+        await self.bot.whisper("募集を終了しました。")
+        sock.close()
 
     @commands.command(pass_context=True)
     async def client(self, ctx, *comment):
