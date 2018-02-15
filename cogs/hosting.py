78bb5960be545cdad9050cb086af9418bf75fa56
starnak <stip2222@gmail.com>
2018-02-15 19:01:44 +0900

    ADD:ソクロール専用rhostコマンド、共通処理のhosting

--- cogs/hosting.py
+++ cogs/hosting.py
@@ -38,8 +38,6 @@ class EchoClientProtocol:
         seconds = (now - self.start_date).seconds
         time = "{0}m{1}s ".format(int(seconds/60), seconds%60)
 
-        print(time)
-        print(status)
         if status == (True, False, True):
             self.count = 0
             status_time_host_message = ":o: :eye: " + (time) + self.host_message
@@ -53,7 +51,7 @@ class EchoClientProtocol:
             status_time_host_message = ":crossed_swords: " + (time) + self.host_message
             discord.compat.create_task(self.bot.edit_message(self.message, status_time_host_message), loop=self.loop)
         else:
-            self.count += 1
+            pass
 
     def error_received(self, exc):
         self.count += 1
@@ -81,8 +79,34 @@ class Hosting(CogMixin):
     async def host(self, ctx, ip_port: str, *comment):
         """
         #holtlistに対戦募集を投稿します。
-        投稿は60分経過か、closeで消去されます。
-        もう一度呼ぶと古い投稿は削除され、新しい内容が投稿されます。
+        募集例「!host 123.456.xxx.xxx:10800 霊夢　レート1500　どなたでもどうぞ！」
+        """
+        normalized_host = unicodedata.normalize('NFKC', ip_port)
+        ip, port = normalized_host.split(":")
+        user = ctx.message.author
+        ip_port_comments = normalized_host + " | " + " ".join(comment)
+        host_message = "{0.mention}, {1}".format(user, ip_port_comments)
+        hostlist_ch = discord.utils.get(self.bot.get_all_channels(),
+                                           name="hostlist")
+
+        not_private = not ctx.message.channel.is_private
+        if not_private:
+            await self.bot.delete_message(ctx.message)
+            raise errors.OnlyPrivateMessage
+
+        await self._delete_messages_from(hostlist_ch, user)
+        await self.bot.whisper("募集を開始しました。")
+        message = await self.bot.send_message(hostlist_ch, host_message, is_sokuroll=False)
+
+        await self._hosting((ip, int(port)), host_message, message)
+
+        await self._delete_messages_from(hostlist_ch, user)
+        await self.bot.whisper("募集を終了しました。")
+
+    @commands.command(pass_context=True)
+    async def rhost(self, ctx, ip_port: str, *comment):
+        """
+        #holtlistにsokuroll有の対戦募集を投稿します。
         募集例「!host 123.456.xxx.xxx:10800 霊夢　レート1500　どなたでもどうぞ！」
         """
         normalized_host = unicodedata.normalize('NFKC', ip_port)
@@ -102,9 +126,20 @@ class Hosting(CogMixin):
         await self.bot.whisper("募集を開始しました。")
         message = await self.bot.send_message(hostlist_ch, host_message)
 
+        await self._hosting((ip, int(port)), ":regional_indicator_r: " + host_message, message, is_sokuroll=True)
+
+        await self._delete_messages_from(hostlist_ch, user)
+        await self.bot.whisper("募集を終了しました。")
+
+
+    async def _hosting(self, addr, host_message, message, is_sokuroll):
+        if is_sokuroll:
+            send_data = PACKET_TO_SOKUROLL
+        else:
+            send_data = PACKET_TO_HOST
         connect = self.bot.loop.create_datagram_endpoint(
             lambda: EchoClientProtocol(self.bot, host_message, message),
-            remote_addr=(ip, port)
+            remote_addr=addr
         )
         transport, protocol = await connect
         while protocol.count <= 10:
@@ -116,9 +151,6 @@ class Hosting(CogMixin):
                 discord.compat.create_task(self.bot.edit_message(protocol.message, status_host_message), loop=protocol.loop)
             protocol.count += 1
         transport.close()
-
-        await self._delete_messages_from(hostlist_ch, user)
-        await self.bot.whisper("募集を終了しました。")
 
     async def _delete_messages_from(self, channel: discord.Channel, user: discord.User):
         async for message in self.bot.logs_from(channel):
