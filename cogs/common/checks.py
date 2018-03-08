c8c4f5f36e0e91edb5371a43ed02aefc2c59577a
starnak <stip2222@gmail.com>
2018-03-09 00:58:21 +0900

    :hammer: DMからの利用を禁止するcheckを追加。

--- cogs/common/checks.py
+++ cogs/common/checks.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from .errors import OnlyPrivateMessage
+from . import errors
 
 
 def _check_manager(ctx):
@@ -8,14 +8,28 @@ def _check_manager(ctx):
         return False
     return "manager" in map(str, ctx.message.author.roles)
 
+
 def is_manager():
     return commands.check(_check_manager)
+
 
 def _check_private(ctx):
     channel = ctx.message.channel
     if not channel.is_private:
-        raise OnlyPrivateMessage
+        raise errors.OnlyPrivateMessage
     return True
+
 
 def only_private():
     return commands.check(_check_private)
+
+
+def _check_not_private(ctx):
+    channel = ctx.message.channel
+    if channel.is_private:
+        raise commands.NoPrivateMessage
+    return True
+
+
+def no_private():
+    return commands.check(_check_not_private)
