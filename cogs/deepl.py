5e298a02dedc5f9bf5d1b5233f9f12ff685a363a
starnak <stip2222@gmail.com>
2021-05-02 12:56:24 +0900

    編集の再翻訳に対応

--- cogs/deepl.py
+++ cogs/deepl.py
@@ -34,6 +34,46 @@ class Deepl(CogMixin, commands.Cog):
         self.bot = bot
         self.jp_en_ch = None
         self.en_jp_ch = None
+        self.send_ids = {}
+
+    @commands.Cog.listener(name='on_message_edit')
+    async def re_translate(self, before, after):
+        if after.author.bot:
+            return
+        if after.id not in self.send_ids:
+            return
+
+        if self.jp_en_ch is None:
+            self.jp_en_ch = discord.utils.get(self.bot.get_all_channels(), name="jp-en")
+            self.jp_en_hook = (await self.jp_en_ch.webhooks())[0]
+        if self.en_jp_ch is None:
+            self.en_jp_ch = discord.utils.get(self.bot.get_all_channels(), name="en-jp")
+            self.en_jp_hook = (await self.en_jp_ch.webhooks())[0]
+
+        if after.channel == self.jp_en_ch:
+            try:
+                json_response = translate(after.content, "EN")
+            except Exception:
+                await self.en_jp_ch.send('May be API limit')
+                await self.jp_en_ch.send('APILimitかもしれません')
+                return
+            translated_text = json_response["translations"][0]["text"]
+            await self.en_jp_hook.edit_message(self.send_ids[after.id],
+                                               content=translated_text,
+                                               username=after.author.name,
+                                               avatar_url=after.author.avatar_url)
+        elif after.channel == self.en_jp_ch:
+            try:
+                json_response = translate(after.content, 'JA')
+            except Exception:
+                await self.en_jp_ch.send('May be API limit')
+                await self.jp_en_ch.send('APILimitかもしれません')
+                return
+            translated_text = json_response["translations"][0]["text"]
+            await self.jp_en_hook.edit_message(self.send_ids[after.id],
+                                               content=translated_text,
+                                               username=after.author.name,
+                                               avatar_url=after.author.avatar_url)
 
     @commands.Cog.listener(name='on_message')
     async def translate(self, message):
@@ -54,10 +94,15 @@ class Deepl(CogMixin, commands.Cog):
                 await self.jp_en_ch.send('APILimitかもしれません')
                 return
             translated_text = json_response["translations"][0]["text"]
-            await self.en_jp_hook.send(content=translated_text,
-                                       username=message.author.name,
-                                       avatar_url=message.author.avatar_url)
-        if message.channel == self.en_jp_ch:
+            sended = await self.en_jp_hook.send(content=translated_text,
+                                                wait=True,
+                                                username=message.author.name,
+                                                avatar_url=message.author.avatar_url)
+            self.send_ids[message.id] = sended.id
+            if len(self.send_ids) > 100:
+                min_id = min(self.send_ids.keys())
+                del self.send_ids[min_id]
+        elif message.channel == self.en_jp_ch:
             try:
                 json_response = translate(message.content, 'JA')
             except Exception:
@@ -65,6 +110,11 @@ class Deepl(CogMixin, commands.Cog):
                 await self.jp_en_ch.send('APILimitかもしれません')
                 return
             translated_text = json_response["translations"][0]["text"]
-            await self.jp_en_hook.send(content=translated_text,
-                                       username=message.author.name,
-                                       avatar_url=message.author.avatar_url)
+            sended = await self.jp_en_hook.send(content=translated_text,
+                                                wait=True,
+                                                username=message.author.name,
+                                                avatar_url=message.author.avatar_url)
+            self.send_ids[message.id] = sended.id
+            if len(self.send_ids) > 100:
+                min_id = min(self.send_ids.keys())
+                del self.send_ids[min_id]
